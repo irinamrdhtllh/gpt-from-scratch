2ec7c4fd82664ce890026e1dd342783f879690b6
Irina M. S <irinamardhatillah@gmail.com>
2024-02-08 22:56:30 +0700

    Add a method to load data for training or validation

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import torch
 
+torch.manual_seed(1337)
+
+
+BATCH_SIZE = 4  # Number of independent sequences will be processed in parallel
+BLOCK_SIZE = 8  # Max context length for predictions
+
+
+def load_data(mode="train"):
+    # Generate a small batch of data with input x and target y
+    if mode == "train":
+        data = train_data
+    elif mode == "val":
+        data = val_data
+
+    ids = torch.randint(
+        len(data) - BLOCK_SIZE, (BATCH_SIZE,)
+    )  # id of the first character of x
+    x = torch.stack([data[i : i + BLOCK_SIZE] for i in ids])
+    y = torch.stack([data[i + 1 : i + BLOCK_SIZE + 1] for i in ids])
+
+    return x, y
+
 
 if __name__ == "__main__":
     with open("./input.txt", "r", encoding="utf-8") as f:
@@ -25,3 +47,11 @@ if __name__ == "__main__":
     n = int(0.9 * len(data))
     train_data = data[:n]
     val_data = data[n:]
+
+    x, y = load_data(mode="train")
+
+    for batch in range(BATCH_SIZE):
+        for block in range(BLOCK_SIZE):
+            context = x[batch, : block + 1]
+            target = y[batch, block]
+            print(f"when the input is {context}, the target is {target}")
